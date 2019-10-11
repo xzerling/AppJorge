43f547b6526782c10520c431125471ed7e9e0cd8
Alvaro <aelguedaicc@gmail.com>
2019-10-11 17:40:12 -0300

    Bd Connection Fix

--- S/R.php
+++ S/R.php
@@ -14,7 +14,7 @@ $N = $_GET['N'];
 if($N === '1'){
 
 
-$result = mysqli_query("SELECT * FROM `NS1`", $db_connection);
+$result = mysqli_query("SELECT * FROM `ns1`", $db_connection);
 
 if (!$db_connection) {
     die('No se ha podido conectar a la base de datos');
@@ -28,7 +28,7 @@ $I = $_GET['I'];
 $O = $_GET['O'];
 
 
-$query = "INSERT INTO`NS1`(`M`,`T`,`H`,`I`,`O`)VALUES('$M','$T','$H','$I','$O')";
+$query = "INSERT INTO`ns1`(`M`,`T`,`H`,`I`,`O`)VALUES('$M','$T','$H','$I','$O')";
 $retry_value = mysqli_query($db_connection,$query);
 if (!$retry_value) {
     die('Error: ' . mysqli_error($db_connection));
@@ -40,7 +40,7 @@ printf('Datos enviados a: NS1');
 if($N === '2'){
 
 
-$result = mysqli_query("SELECT * FROM `NS2`", $db_connection);
+$result = mysqli_query("SELECT * FROM `ns2`", $db_connection);
 
 if (!$db_connection) {
     die('No se ha podido conectar a la base de datos');
@@ -53,7 +53,7 @@ $I = $_GET['I'];
 $O = $_GET['O'];
 
 
-$query = "INSERT INTO`NS2`(`M`,`T`,`H`,`I`,`O`)VALUES('$M','$T','$H','$I','$O')";
+$query = "INSERT INTO`ns2`(`M`,`T`,`H`,`I`,`O`)VALUES('$M','$T','$H','$I','$O')";
 $retry_value = mysqli_query($db_connection,$query);
 if (!$retry_value) {
     die('Error: ' . mysqli_error($db_connection));
@@ -66,7 +66,7 @@ printf('Datos enviados a: NS2');
 if($N === '3'){
 
 
-$result = mysqli_query("SELECT * FROM `NS3`", $db_connection);
+$result = mysqli_query("SELECT * FROM `ns3`", $db_connection);
 if (!$db_connection) {
     die('No se ha podido conectar a la base de datos');
 }
@@ -78,7 +78,7 @@ $I = $_GET['I'];
 $O = $_GET['O'];
 
 
-$query = "INSERT INTO`NS3`(`M`,`T`,`H`,`I`,`O`)VALUES('$M','$T','$H','$I','$O')";
+$query = "INSERT INTO`ns3`(`M`,`T`,`H`,`I`,`O`)VALUES('$M','$T','$H','$I','$O')";
 $retry_value = mysqli_query($db_connection,$query);
 if (!$retry_value) {
     die('Error: ' . mysqli_error($db_connection));
@@ -92,7 +92,7 @@ printf('Datos enviados a: NS3');
 if($N === '4'){
 
 
-$result = mysqli_query("SELECT * FROM `NS4`", $db_connection);
+$result = mysqli_query("SELECT * FROM `ns4`", $db_connection);
 
 if (!$db_connection) {
     die('No se ha podido conectar a la base de datos');
@@ -120,7 +120,7 @@ printf('Datos enviados a: NS4');
 if($N === '5'){
 
 
-$result = mysqli_query("SELECT * FROM `NS5`", $db_connection);
+$result = mysqli_query("SELECT * FROM `ns5`", $db_connection);
 
 if (!$db_connection) {
     die('No se ha podido conectar a la base de datos');
@@ -134,7 +134,7 @@ $I = $_GET['I'];
 $O = $_GET['O'];
 
 
-$query = "INSERT INTO`NS5`(`M`,`T`,`H`,`I`,`O`)VALUES('$M','$T','$H','$I','$O')";
+$query = "INSERT INTO`ns5`(`M`,`T`,`H`,`I`,`O`)VALUES('$M','$T','$H','$I','$O')";
 $retry_value = mysqli_query($db_connection,$query);
 if (!$retry_value) {
     die('Error: ' . mysqli_error($db_connection));
@@ -149,7 +149,7 @@ printf('Datos enviados a: NS5');
 if($N === '6'){
 
 
-$result = mysqli_query("SELECT * FROM `NS6`", $db_connection);
+$result = mysqli_query("SELECT * FROM `ns6`", $db_connection);
 
 if (!$db_connection) {
     die('No se ha podido conectar a la base de datos');
@@ -163,7 +163,7 @@ $I = $_GET['I'];
 $O = $_GET['O'];
 
 
-$query = "INSERT INTO`NS6`(`M`,`T`,`H`,`I`,`O`)VALUES('$M','$T','$H','$I','$O')";
+$query = "INSERT INTO`ns6`(`M`,`T`,`H`,`I`,`O`)VALUES('$M','$T','$H','$I','$O')";
 $retry_value = mysqli_query($db_connection,$query);
 if (!$retry_value) {
     die('Error: ' . mysqli_error($db_connection));
--- S/dataNS6.php
+++ S/dataNS6.php
@@ -1,11 +1,11 @@
 <?php
 header('Content-Type: application/json');
-$con = mysqli_connect("localhost", "root", "Jorge1995", "db_prueba");
+$con = mysqli_connect("citra-test.cmmqgr4kvqxz.us-east-2.rds.amazonaws.com", "admin", "12345678", "citra");
 if (mysqli_connect_errno($con)) {
     echo "Failed to connect to DataBase: " . mysqli_connect_error();
 } else {
     $data_points = array();
-    $result = mysqli_query($con, "SELECT * FROM NS6"); 
+    $result = mysqli_query($con, "SELECT * FROM ns6"); 
     while ($row = mysqli_fetch_array($result)) {
         $point = array("M" => $row['M'],"T" => $row['T'], "H" => $row['H'], "I" => $row['I'], "O" => $row['O']);
         array_push($data_points, $point);
